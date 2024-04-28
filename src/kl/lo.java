aafe1b2728e657bfb702fbc0e43d6d6b37d36fc0
Glenn <casetoolbca201@gmail.com>
2024-04-29 00:53:06 +0530

    f2

--- src/kl/lo.java
+++ src/kl/lo.java
@@ -5,6 +5,9 @@ public class lo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		System.out.println("you suck");
+		
 
 	}
 
